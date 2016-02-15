86485b05556d5b8f83d7bdb4ab74bc31ecd28f58
Peter Lollo <peteralollo@gmail.com>
2016-02-14 17:49:41 -0800

    Add comments to enhance transferrability of scaffolding

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,13 @@
+
+// Rename main module and require more as needed
 angular.module('mean', ['mean.home', 'ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
+    //Set up Appropriate routes here
       .when('/home', {
         templateUrl: 'app/home/home.html',
         controller: 'HomeController'
       });
+      //Require and add $httpProvider interceptor here if needed
   }]);
+  //Add authorization checks here if needed
--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,16 +1,8 @@
 angular.module('mean.home', [])
   .controller('HomeController', function($scope, $http){
+
    var init = function() {console.log('home controller up and going');};
+   
    init();
 
-   $scope.postIt = function() {
-    return $http({
-      method: 'POST',
-      url: '/home'
-    }).then(function(res){
-      console.log('data from post?', res);
-    }).catch(function(error) {
-      console.log('error from post?', error);
-    });
-   };
   });
