52a24ba0bcd928f7a8b3f6270f8cfc632e5c59a4
Peter Lollo <peteralollo@gmail.com>
2016-02-14 17:57:10 -0800

    Add template for angular factories and services

---
+++ client/app/services/services.js
@@ -0,0 +1,7 @@
+//Be sure to rename all angular modules, rename file structure, and reference the proper directories & files in the index.hmtl page
+angular.module('mean.services', [])
+  .factory('SomeFactory', function() {
+    //inject dependencies as arguments to function
+    //declare helper functions/services
+    //return an object with the functions as key-value pairs
+  });
