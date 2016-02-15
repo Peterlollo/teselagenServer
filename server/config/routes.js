4031376e2691b956d0df2bc275fc8fd68bb39f6a
Peter Lollo <peteralollo@gmail.com>
2016-02-14 18:13:25 -0800

    Add gruntfile and template for mongoose controller and models

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,21 @@
 
 
 module.exports = function(app, express) {
+
+  
+  //fill in server side functionality here
+
+
   app.get('/users', function(req, res, err) {
     console.log('you tryin to get users? w/this req.body', req.body);
     res.send(200);
   });
+
+
   app.post('/home', function(req, res, err){
     console.log('tryin to post to users with this req.body', req.body);
     res.send({'hey': 'man'});
   });
+
+
 };
